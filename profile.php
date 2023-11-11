8bcb98e5c156794ec299a7199e5870a56c086c4a
andrewchristoperr <andrewch03.ac@gmail.com>
2023-11-11 21:56:44 +0700

    a

--- profile.php
+++ profile.php
@@ -114,147 +114,118 @@
     }
 
     .custom-cross-icon::before {
-      content: "✖"; /* Unicode karakter untuk centang (checkmark) */
-      font-size: 24px; /* Sesuaikan ukuran ikon sesuai kebutuhan */
+      content: "✖";
+      /* Unicode karakter untuk centang (checkmark) */
+      font-size: 24px;
+      /* Sesuaikan ukuran ikon sesuai kebutuhan */
     }
-
   </style>
 </head>
 
 <body>
 
-  <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top">
-    <div class="container d-flex align-items-center justify-content-between position-relative">
+  <?php include "template/header.php"; ?>
 
-      <div class="logo">
-        <h1 class="text-light"><a href="index.html"><span>UMKMANJALITA</span></a></h1>
-      </div>
 
-      <nav id="navbar" class="navbar">
-        <ul>
-          <li><a class="nav-link scrollto" href="index.html">Home</a></li>
-          <li><a class="nav-link scrollto" href="#">Favourite</a></li>
-          <li><a class="nav-link scrollto" href="#">Account</a></li>
-          <li>
-            <div class="box">
-              <div class="container-4">
-                <input type="search" id="search" placeholder="Search..." />
-                <button class="icon"><i class="fa fa-search"></i></button>
-              </div>
-            </div>
-          </li>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
-
-    </div>
-  </header><!-- End Header -->
-
-  
   <main id="main">
-  
+
     <section id="akun">
       <div class="container">
         <div class="row">
           <div class="col-lg-2 content d-flex justify-content-center">
             <img src="assets/img/testimonials/testimonials-2.jpg" class="profil-img" alt="">
-            
+
           </div>
-  
+
           <div class="col-lg-2 text-center">
             <div class="profil-nama centered-text">
               Hi, Budi!
             </div>
           </div>
-  
+
         </div>
       </div>
     </section>
 
     <!-- ======= Portfolio Section ======= -->
-      <div class="container">
+    <div class="container">
 
-        <div class="section-title" data-aos="fade-in" data-aos-delay="100">
-          <h2>My UMKM</h2>
-        </div>
+      <div class="section-title" data-aos="fade-in" data-aos-delay="100">
+        <h2>My UMKM</h2>
+      </div>
 
 
-        <div class="row portfolio" data-aos="fade-up">
-          
-          <div class="container">
-            <div class="scrollable-container">
+      <div class="row portfolio" data-aos="fade-up">
 
-              <div class="col-lg-4 col-md-6 col-sm-12 p-3 portfolio-item filter-app">
-                <div class="card card-block">
-                  <div class="portfolio-wrap">
-                    <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
-                    <div class="portfolio-links">
-                      <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery"
-                        class="portfolio-lightbox" title="App 1"><i class="custom-cross-icon"></i></a>
-                      <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
-                    </div>
+        <div class="container">
+          <div class="scrollable-container">
+
+            <div class="col-lg-4 col-md-6 col-sm-12 p-3 portfolio-item filter-app">
+              <div class="card card-block">
+                <div class="portfolio-wrap">
+                  <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
+                  <div class="portfolio-links">
+                    <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 1"><i class="custom-cross-icon"></i></a>
+                    <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
                   </div>
                 </div>
               </div>
-  
-              <div class="col-lg-4 col-md-6 col-sm-12 p-3 portfolio-item filter-app">
-                <div class="card card-block">
-                  <div class="portfolio-wrap">
-                    <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
-                    <div class="portfolio-links">
-                      <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery"
-                        class="portfolio-lightbox" title="App 1"><i class="custom-cross-icon"></i></a>
-                      <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-  
-              <div class="col-lg-4 col-md-6 col-sm-12 p-3 portfolio-item filter-app">
-                <div class="card card-block">
-                  <div class="portfolio-wrap">
-                    <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
-                    <div class="portfolio-links">
-                      <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery"
-                        class="portfolio-lightbox" title="App 1"><i class="custom-cross-icon"></i></a>
-                      <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-  
-              <div class="col-lg-4 col-md-6 col-sm-12 p-3 portfolio-item filter-app">
-                <div class="card card-block">
-                  <div class="portfolio-wrap">
-                    <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
-                    <div class="portfolio-links">
-                      <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery"
-                        class="portfolio-lightbox" title="App 1"><i class="custom-cross-icon"></i></a>
-                      <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-  
-              <div class="col-lg-4 col-md-6 col-sm-12 p-3 portfolio-item filter-app">
-                <a href="register.php">
-                  <div class="card card-addnew">
-                    <h3 class="text-center">
-                      Add New UMKM
-                    </h3>
-                  </div>
-                </a>
-              </div>
-  
             </div>
-          </div>
-          
 
+            <div class="col-lg-4 col-md-6 col-sm-12 p-3 portfolio-item filter-app">
+              <div class="card card-block">
+                <div class="portfolio-wrap">
+                  <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
+                  <div class="portfolio-links">
+                    <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 1"><i class="custom-cross-icon"></i></a>
+                    <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-sm-12 p-3 portfolio-item filter-app">
+              <div class="card card-block">
+                <div class="portfolio-wrap">
+                  <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
+                  <div class="portfolio-links">
+                    <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 1"><i class="custom-cross-icon"></i></a>
+                    <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-sm-12 p-3 portfolio-item filter-app">
+              <div class="card card-block">
+                <div class="portfolio-wrap">
+                  <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
+                  <div class="portfolio-links">
+                    <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 1"><i class="custom-cross-icon"></i></a>
+                    <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-sm-12 p-3 portfolio-item filter-app">
+              <a href="register.php">
+                <div class="card card-addnew">
+                  <h3 class="text-center">
+                    Add New UMKM
+                  </h3>
+                </div>
+              </a>
+            </div>
+
+          </div>
         </div>
+
 
       </div>
-      <!--End Portfolio Section -->
+
+    </div>
+    <!--End Portfolio Section -->
 
     <div class="container mb-3 mt-4">
       <div class="row">
@@ -269,55 +240,55 @@
       </div>
     </div>
 
-        <!-- Modal -->
-        <div class="modal fade" id="help" tabindex="-1" role="dialog" aria-labelledby="helpTitle" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content rounded-0">
-              <div class="modal-body p-4 px-5">
-    
-                
-                <div class="main-content text-center">
-                    
-                    <a href="#" class="close-btn" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true"><span class="icon-close2"></span></span>
-                    </a>
-                    
-                    <label for="">Pilih UMKM Anda</label>
-                    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi non illum necessitatibus vitae rerum minima sint assumenda fugit delectus. Asperiores enim praesentium, amet quae doloremque necessitatibus provident dolorem libero modi.</p>
-    
-                    <div class="dropdown">
-                      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Pilih UMKM Anda
-                      </button>
-                      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <a class="dropdown-item" href="#">Pandan Wangi</a>
-                        <a class="dropdown-item" href="#">UMKM 1</a>
-                        <a class="dropdown-item" href="#">UMKM 2</a>
-                      </div>
-                    </div>
-    
-                    <br><br>
-    
-                    <form action="#">
-                      <label for="">Masukkan Nominal</label>
-                      <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi non illum necessitatibus vitae rerum minima sint assumenda fugit delectus. Asperiores enim praesentium, amet quae doloremque necessitatibus provident dolorem libero modi.</p>
-                      <div class="form-group mb-4">
-                        <input type="text" class="form-control text-center" placeholder="Masukkan Nominal">
-                      </div>
-    
-                      <div class="d-flex">
-                        <div class="mx-auto">
-                        <a href="#" class="btn btn-primary">Ajukan</a>
-                        </div>
-                      </div>
-                    </form>
-                  
+    <!-- Modal -->
+    <div class="modal fade" id="help" tabindex="-1" role="dialog" aria-labelledby="helpTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content rounded-0">
+          <div class="modal-body p-4 px-5">
+
+
+            <div class="main-content text-center">
+
+              <a href="#" class="close-btn" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true"><span class="icon-close2"></span></span>
+              </a>
+
+              <label for="">Pilih UMKM Anda</label>
+              <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi non illum necessitatibus vitae rerum minima sint assumenda fugit delectus. Asperiores enim praesentium, amet quae doloremque necessitatibus provident dolorem libero modi.</p>
+
+              <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Pilih UMKM Anda
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a class="dropdown-item" href="#">Pandan Wangi</a>
+                  <a class="dropdown-item" href="#">UMKM 1</a>
+                  <a class="dropdown-item" href="#">UMKM 2</a>
                 </div>
-    
               </div>
+
+              <br><br>
+
+              <form action="#">
+                <label for="">Masukkan Nominal</label>
+                <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi non illum necessitatibus vitae rerum minima sint assumenda fugit delectus. Asperiores enim praesentium, amet quae doloremque necessitatibus provident dolorem libero modi.</p>
+                <div class="form-group mb-4">
+                  <input type="text" class="form-control text-center" placeholder="Masukkan Nominal">
+                </div>
+
+                <div class="d-flex">
+                  <div class="mx-auto">
+                    <a href="#" class="btn btn-primary">Ajukan</a>
+                  </div>
+                </div>
+              </form>
+
             </div>
+
           </div>
-        </div> <!-- End Modal -->
+        </div>
+      </div>
+    </div> <!-- End Modal -->
 
   </main><!-- End #main -->
 
@@ -338,8 +309,8 @@
               </p>
               <div class="social-links mt-3">
                 <a href="https://twitter.com/info_surabaya?spm=a2o4j.home.sns.d_twr.579953e0jUysxl" target="_blank" class="twitter"><i class="bx bxl-twitter"></i></a>
-                <a href="https://www.facebook.com/kecamatangayungan?spm=a2o4j.home.sns.d_fbk.579953e0jUysxl" target="_blank"" class="facebook"><i class="bx bxl-facebook"></i></a>
-                <a href="https://www.instagram.com/kecamatan.tandes/?spm=a2o4j.home.sns.d_ins.579953e0jUysxl" target="_blank"" class="instagram"><i class="bx bxl-instagram"></i></a>
+                <a href="https://www.facebook.com/kecamatangayungan?spm=a2o4j.home.sns.d_fbk.579953e0jUysxl" target="_blank"" class=" facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="https://www.instagram.com/kecamatan.tandes/?spm=a2o4j.home.sns.d_ins.579953e0jUysxl" target="_blank"" class=" instagram"><i class="bx bxl-instagram"></i></a>
                 <a href="#" class="google-plus"><i class="bx bxl-whatsapp"></i></a>
               </div>
             </div>
@@ -394,8 +365,7 @@
     </div>
   </footer><!-- End Footer -->
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
-      class="bi bi-arrow-up-short"></i></a>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
   <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
@@ -411,7 +381,7 @@
   <script src="js/jquery-3.3.1.min.js"></script>
   <script src="js/popper.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
-  <script src="js/main.js"></script>  
+  <script src="js/main.js"></script>
 
 </body>
 
