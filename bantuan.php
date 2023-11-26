2ed9e43277ce865b2421807d738ab81043d1bc58
emilyjoyceline <c14210077@john.petra.ac.id>
2023-11-26 16:52:35 +0700

    nambah"i form bantuan
    blm coba connect backend

--- bantuan.php
+++ bantuan.php
@@ -14,7 +14,9 @@
     <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
 
     <!-- Vendor CSS Files -->
     <link href="assets/vendor/aos/aos.css" rel="stylesheet">
@@ -29,10 +31,14 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
     <!-- JQuery -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"
+        integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- Sweet Alert -->
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.js"></script>
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css" id="theme-styles">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 
     <style>
         #footer {
@@ -82,7 +88,7 @@
             border-bottom-width: 2px;
         }
 
-        label { 
+        label {
             font-family: "Raleway", sans-serif;
             font-weight: 600;
         }
@@ -96,6 +102,17 @@
 
         .hidden {
             display: none;
+        }
+
+        .required::after {
+            content: "*";
+            color: red;
+        }
+
+        .explanation {
+            font-size: 12px;
+            color: rgba(0, 0, 0, 0.7);
+            font-style: italic;
         }
     </style>
 </head>
@@ -118,50 +135,76 @@
                             <form class="row d-flex justify-content-center" action="#" method="post">
                                 <div class="col-md-10">
                                     <div class="mb-4">
-                                        <label for="formFile" class="form-label">Alasan Permintaan Bantuan</label>
-                                        <input class="form-control" type="text" id="tujuanBantuan" name="tujuanBantuan" required >
+                                        <label for="formFile" class="required">Alasan Pengajuan Bantuan</label>
+                                        <textarea class="form-control" aria-label="alasan_pengajuan"></textarea>
+                                        <span class="explanation" italic>Contoh: Saya mengajukan bantuan ini untuk ...
+                                            Selama mengoperasikan usaha terdapat kendala ... Dalam rangka bazzar 17
+                                            Agustus di kecamatan X, saya ingin memperbaiki ...</span>
+
                                     </div>
 
                                     <div class="mb-4">
-                                        <label for="formFile" class="form-label">Dokumen Pendukung</label>
-                                        <input class="form-control" type="text" id="pendapatan" name="pendapatan" required>
+                                        <label for="formFile" class="required">Dokumen Pendukung</label>
+                                        <input class="form-control" type="text" id="pendapatan" name="pendapatan"
+                                            required>
+                                        <span class="explanation" italic>Contoh: Foto Tempat Usaha, Foto Produk,
+                                            Proposal Pengajuan, Surat Keterangan Lainnya</span>
+
                                     </div>
 
                                     <div class="mb-4">
-                                        <label for="formFile" class="form-label">Kebutuhan</label>
+                                        <label for="formFile" class="required">Bantuan yang Dibutuhkan</label>
+
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" value="" id="danaCheckbox"
+                                                onchange="toggleInput()">
+                                            <label class="form-check-label" for="danaCheckbox">Dana</label>
+                                        </div>
+                                        <div id="danaInput" class="form-group mt-2 mb-2 hidden">
+                                            <!-- nominal yang Dibutuhkan -->
+                                            <label for="formFile" class="required">Nominal yang Dibutuhkan</label>
+                                            <input class="form-control" type="number" id="Rp" name="nominal"
+                                                pattern="^\Rp\d{1,3}(,\d{3})*(\.\d+)?Rp" required placeholder Rp 1000.000>
+
+                                            <label for="formFile" class="required">Rincian Rencana Penggunaan
+                                                Dana</label>
+                                            <textarea class="form-control" aria-label="rincian_dana"></textarea>
+                                            <span class="explanation" italic>Contoh: 1. Membeli bahan baku sebanyak ...
+                                                kg seharga Rp ... </span>
+                                        </div>
+
                                         <div class="form-check">
                                             <input class="form-check-input" type="checkbox" value="" id="tendaCheckbox">
                                             <label class="form-check-label" for="tendaCheckbox">Tenda</label>
                                         </div>
                                         <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" value="" id="gerobakCheckbox">
+                                            <input class="form-check-input" type="checkbox" value=""
+                                                id="gerobakCheckbox">
                                             <label class="form-check-label" for="gerobakCheckbox">Gerobak</label>
                                         </div>
 
                                         <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" value="" id="spandukCheckbox">
+                                            <input class="form-check-input" type="checkbox" value=""
+                                                id="spandukCheckbox">
                                             <label class="form-check-label" for="spandukCheckbox">Spanduk</label>
                                         </div>
+
+
                                         <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" value="" id="danaCheckbox" onchange="toggleInput()">
-                                            <label class="form-check-label" for="danaCheckbox">Dana</label>
-                                        </div>
-                                        <!-- Input Dana -->
-                                        <div id="danaInput" class="form-group mt-2 mb-2 hidden">
-                                            <input class="form-control" type="text" id="danaAmount" name="danaAmount" placeholder="Masukkan Nominal">
-                                        </div>
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" value="" id="lainnyaCheckbox" onchange="toggleInput()">
+                                            <input class="form-check-input" type="checkbox" value=""
+                                                id="lainnyaCheckbox" onchange="toggleInput()">
                                             <label class="form-check-label" for="lainnyaCheckbox">Lainnya</label>
                                         </div>
                                         <!-- Input Lainnya -->
                                         <div id="lainnyaInput" class="form-group mt-2 mb-2 hidden">
-                                            <input class="form-control" type="text" id="lainnya" name="lainnya" placeholder="Masukkan Kebutuhan Lainnya">
+                                        <label for="formFile" class="form">Kebutuhan Lainnya</label>
+                                            <input class="form-control" type="text" id="lainnya" name="lainnya">
                                         </div>
                                     </div>
 
                                     <div class="d-grid mx-auto">
-                                        <button class="button" type="submit" id="submit" name="submit">Ajukan Bantuan</button>
+                                        <button class="button" type="submit" id="submit" name="submit">Ajukan
+                                            Bantuan</button>
                                     </div>
                                 </div>
 
@@ -180,7 +223,8 @@
     <?php include "template/footer.php"; ?>
     <!-- End Footer -->
 
-    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
 
     <!-- Vendor JS Files -->
     <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
@@ -212,6 +256,17 @@
             } else {
                 lainnyaInput.classList.add('hidden');
             }
+        }
+
+        function Rp(input, blur) {
+            var p = input.value.replace(/[^\d]/g, ""),
+                locale = 'id-ID',
+                value = (p === "" || p == null) ? "" : Number(p),
+                formatted = (value === 0) ? "" : value.toLocaleString(locale, {
+                    style: "currency",
+                    currency: "IDR"
+                });
+            input.value = formatted;
         }
     </script>
 </body>
