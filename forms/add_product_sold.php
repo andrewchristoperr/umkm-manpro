17efa5e819a34a78739ce8a7c01a0925e7b9c009
prcillias <pricilliasilfany22@gmail.com>
2023-12-02 15:35:38 +0700

    tambah penjualan produk

---
+++ forms/add_product_sold.php
@@ -0,0 +1,21 @@
+<?php
+require '../connect.php';
+session_start();
+
+$umkm_id = $_POST['id'];
+$product_name = $_POST['namaProduk'];
+$jumlah_terjual = $_POST['jumlahProdukTerjual'];
+
+$query = "SELECT id FROM umkm_products WHERE name = ?";
+$search = $conn->prepare($query);
+$search->execute([$product_name]);
+$product_id = $search->fetch();
+$product_id = (int)$product_id;
+
+$insert = "INSERT INTO umkm_products_sold (umkm_id, product_id, product_name, jumlah_terjual) VALUES ('$umkm_id', '$product_id', '$product_name', '$jumlah_terjual')";
+$insert = $conn->query($insert);
+
+$result = 1;
+
+echo $result;
+?>
